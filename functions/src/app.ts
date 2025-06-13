9d8f8aa2b783dbe2c0305bee7cb7d26d3b939e15
Abraham Molina <abraham.molina86@gmail.com>
2025-06-12 22:56:53 -0600

    manejo de cors

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -2,11 +2,19 @@ import express from 'express';
 import taskRoutes from './controllers/task.controller';
 import userRoutes from './controllers/user.controller';
 import * as dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 
 const app = express();
+
+app.use(cors({
+  origin: ['https://frontend-task-webapp.web.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use('/tasks', taskRoutes);
