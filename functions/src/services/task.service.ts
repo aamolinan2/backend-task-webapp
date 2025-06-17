f344fa95aa57a2045ce85ad29a0c3ecd073a9b57
Abraham Molina <abraham.molina86@gmail.com>
2025-06-17 15:07:37 -0600

    se aplica sanitization html para entradas de valore

--- functions/src/services/task.service.ts
+++ functions/src/services/task.service.ts
@@ -2,6 +2,7 @@
 import { Task } from '../models/task.model';
 import { TaskRepository } from '../repositories/task.repository';
 import admin from '../utils/firebase';
+import sanitizeHtml from 'sanitize-html';
 
 export class TaskService {
   private repository = new TaskRepository();
@@ -15,17 +16,27 @@ export class TaskService {
   }
 
   async createTask(userId: string, data: { title: string; description: string }): Promise<Task> {
+    const title = sanitizeHtml(data.title, { allowedTags: [], allowedAttributes: {} });
+    const description = sanitizeHtml(data.description || '', { allowedTags: [], allowedAttributes: {} });
+
     const newTask: Omit<Task, 'id'> = {
-      title: data.title,
-      description: data.description,
+      title,
+      description,
       createdAt: admin.firestore.Timestamp.now(),
       completed: false,
       userId,
     };
+
     return this.repository.create(newTask);
   }
 
   async updateTask(id: string, updates: Partial<Omit<Task, 'id'>>): Promise<Task> {
+    if (updates.title)
+      updates.title = sanitizeHtml(updates.title, { allowedTags: [], allowedAttributes: {} });
+
+    if (updates.description)
+      updates.description = sanitizeHtml(updates.description, { allowedTags: [], allowedAttributes: {} });
+
     await this.repository.update(id, updates);
     const updated = await this.repository.getAllByUser(updates.userId!);
     return updated.find(t => t.id === id)!;
